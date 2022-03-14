24a2ab67de94a8108d43208dd777dba123ce9104
Suraj49 <1507surajkumar@gmail.com>
2022-03-14 10:42:02 +0530

    commit

--- Kth_Smallest_Element_3.java
+++ Kth_Smallest_Element_3.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Kth_Smallest_Element_3 {
@@ -9,18 +10,19 @@ public class Kth_Smallest_Element_3 {
             arr[i]=sc.nextInt();
         }
         int k=sc.nextInt();
-        for (int i = 0; i < arr.length; i++)
-        {
-            for (int j = i + 1; j < arr.length; j++)
-            {
-                if (arr[i] > arr[j])
-                {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
+//        for (int i = 0; i < arr.length; i++)
+//        {
+//            for (int j = i + 1; j < arr.length; j++)
+//            {
+//                if (arr[i] > arr[j])
+//                {
+//                    int temp = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j] = temp;
+//                }
+//            }
+//        }
+        Arrays.sort(arr);
         System.out.println(arr[k-1]);
     }
 }
